eda95ca089795aa86b223642d09758e2f3bce03b
Mat Ryer <mat@stretchr.com>
2014-03-03 16:16:24 -0700

    Initial Commit

---
+++ PathKit/PathKit.h
@@ -0,0 +1,13 @@
+//
+//  PathKit.h
+//  PathKit
+//
+//  Created by Mat Ryer on 3/3/14.
+//  Copyright (c) 2014 Mat Ryer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PathKit : NSObject
+
+@end
---
+++ PathKit/PathKit.m
@@ -0,0 +1,13 @@
+//
+//  PathKit.m
+//  PathKit
+//
+//  Created by Mat Ryer on 3/3/14.
+//  Copyright (c) 2014 Mat Ryer. All rights reserved.
+//
+
+#import "PathKit.h"
+
+@implementation PathKit
+
+@end
